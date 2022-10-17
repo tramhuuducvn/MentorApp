0674ce4c1fdbeb890871980ba333d955ac8e853d
ducth5 <ducth5@vng.com.vn>
2022-10-17 14:56:42 +0700

    add icon

--- android/app/src/main/java/com/mentorapp/MainActivity.java
+++ android/app/src/main/java/com/mentorapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.mentorapp;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -23,6 +24,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new MainActivityDelegate(this, getMainComponentName());
+  }
+
+  // setup follow this guide: https://reactnavigation.org/docs/getting-started
+  @Override
+  protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(null);
   }
 
   public static class MainActivityDelegate extends ReactActivityDelegate {
